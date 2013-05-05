c31322305c683b5fb90219ac8c1c7aa15d1bdff7
Kareem Yousry <kareem.yousrii@gmail.com>
2013-05-05 17:12:32 +0200

    Initial Commit

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('DB_SOCKET', ':/Applications/MAMP/tmp/mysql/mysql.sock');
+define('DB_USR', 'root');
+define('DB_PWD', 'root');
+define('DB_NAME', '');
+define('TEST_FILE', '');
